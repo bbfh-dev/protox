31288e0c65f6c53245c5a6fb92dea47db70ef8a8
BubbleFish <daforsastudia@gmail.com>
2025-06-04 13:46:39 +0300

    test: Add test for writing and reading all kinds of data

--- tests/protox_test.go
+++ tests/protox_test.go
@@ -1,1 +1,76 @@
 package protox_test
+
+import (
+	"bufio"
+	"bytes"
+	"testing"
+
+	"github.com/bbfh-dev/protox"
+	"gotest.tools/assert"
+)
+
+type Example struct {
+	Byte        byte
+	BytesDelim  []byte
+	String      string
+	StringDelim string
+	Int         int64
+	IntDelim    int64
+	FloatDelim  float64
+}
+
+func (example *Example) Protox() *protox.Processor {
+	return protox.New().
+		ThenByte(&example.Byte).
+		ThenBytesDelim(&example.BytesDelim, '\x00').
+		ThenString(&example.String, 3).
+		ThenStringDelim(&example.StringDelim, '\x00').
+		ThenInt(&example.Int).
+		ThenIntDelim(&example.IntDelim, '\x00').
+		ThenFloatDelim(&example.FloatDelim, '\x00').
+		Build()
+}
+
+var example = &Example{
+	Byte:        'g',
+	BytesDelim:  []byte{'a', 'b', 'c'},
+	String:      "hi!",
+	StringDelim: "Hello World!",
+	Int:         1337,
+	IntDelim:    42069,
+	FloatDelim:  420.69,
+}
+
+var exampleString = "g" + "abc\x00" + "hi!" + "Hello World!\x00" + "9\x05\x00\x00\x00\x00\x00\x00" + "42069\x00" + "420.69\x00"
+
+func TestWrite(test *testing.T) {
+	var buffer bytes.Buffer
+	assert.NilError(test, example.Protox().Write(&buffer))
+	test.Logf("Result: %q\n", buffer.String())
+	assert.DeepEqual(test, buffer.String(), exampleString)
+}
+
+func TestRead(test *testing.T) {
+	var buffer bytes.Buffer
+	assert.NilError(test, example.Protox().Write(&buffer))
+
+	instance := &Example{
+		Byte:        0,
+		BytesDelim:  []byte{},
+		String:      "",
+		StringDelim: "",
+		Int:         0,
+		IntDelim:    0,
+		FloatDelim:  0,
+	}
+	test.Logf("Before: %#v\n", instance)
+	assert.NilError(test, instance.Protox().Read(bufio.NewReader(&buffer)))
+	test.Logf("After: %#v\n", instance)
+	assert.DeepEqual(test, instance.Byte, example.Byte)
+	assert.DeepEqual(test, instance.BytesDelim, example.BytesDelim)
+	assert.DeepEqual(test, instance.String, example.String)
+	assert.DeepEqual(test, instance.StringDelim, example.StringDelim)
+	assert.DeepEqual(test, instance.Int, example.Int)
+	assert.DeepEqual(test, instance.IntDelim, example.IntDelim)
+	assert.DeepEqual(test, instance.FloatDelim, example.FloatDelim)
+}
