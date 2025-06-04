ed01c49f55354b4815ae99faac671c8bfa4e874e
BubbleFish <daforsastudia@gmail.com>
2025-06-04 13:13:26 +0300

    feat: Add processor

---
+++ builder.go
@@ -0,0 +1,74 @@
+package protox
+
+import "github.com/bbfh-dev/protox/internal"
+
+type Builder struct {
+	steps []internal.Step
+}
+
+func New() *Builder {
+	return &Builder{
+		steps: []internal.Step{},
+	}
+}
+
+// Build the Protox processor
+func (build *Builder) Build() *Processor {
+	return &Processor{
+		steps: build.steps,
+	}
+}
+
+func (builder *Builder) ThenByte(ref *byte) *Builder {
+	builder.steps = append(builder.steps, &internal.Byte{
+		Ref: ref,
+	})
+	return builder
+}
+
+func (builder *Builder) ThenBytesDelim(value []byte, delim byte) *Builder {
+	builder.steps = append(builder.steps, &internal.ByteDelim{
+		Value: value,
+		Delim: delim,
+	})
+	return builder
+}
+
+func (builder *Builder) ThenString(ref *string, size int) *Builder {
+	builder.steps = append(builder.steps, &internal.String{
+		Ref:  ref,
+		Size: size,
+	})
+	return builder
+}
+
+func (builder *Builder) ThenStringDelim(ref *string, delim byte) *Builder {
+	builder.steps = append(builder.steps, &internal.StringDelim{
+		Ref:   ref,
+		Delim: delim,
+	})
+	return builder
+}
+
+func (builder *Builder) ThenInt(ref *int64) *Builder {
+	builder.steps = append(builder.steps, &internal.Int{
+		Ref: ref,
+	})
+	return builder
+}
+
+func (builder *Builder) ThenIntDelim(ref *int64, delim byte) *Builder {
+	builder.steps = append(builder.steps, &internal.IntDelim{
+		Ref:   ref,
+		Delim: delim,
+	})
+	return builder
+}
+
+func (builder *Builder) ThenFloatDelim(ref *float64, delim byte) *Builder {
+	builder.steps = append(builder.steps, &internal.FloatDelim{
+		Ref:   ref,
+		Delim: delim,
+	})
+	return builder
+}
--- protox.go
+++ protox.go
@@ -1,19 +1,66 @@
 package protox
 
 import (
+	"bufio"
+	"errors"
+	"fmt"
 	"io"
 
-	internal "github.com/bbfh-dev/protox/internal"
+	"github.com/bbfh-dev/protox/internal"
 )
 
-func NewReader(reader io.Reader) *internal.Reader {
-	return &internal.Reader{
-		In: reader,
-	}
+type Processor struct {
+	steps []internal.Step
 }
 
-func NewWriter(writer io.Writer) *internal.Writer {
-	return &internal.Writer{
-		Out: writer,
+// Returns on the first error encountered
+func (processor *Processor) Read(reader *bufio.Reader) error {
+	for i, step := range processor.steps {
+		err := step.Read(reader)
+		if err != nil {
+			return fmt.Errorf("protox.Processor.step(%d): %w", i, err)
+		}
 	}
+
+	return nil
+}
+
+// Continues to read even if an error is encountered, returns all errors combined
+func (processor *Processor) ForceRead(reader *bufio.Reader) error {
+	errs := []error{}
+
+	for i, step := range processor.steps {
+		err := step.Read(reader)
+		if err != nil {
+			errs = append(errs, fmt.Errorf("protox.Processor(reading).step(%d): %w", i, err))
+		}
+	}
+
+	return errors.Join(errs...)
+}
+
+// Returns on the first error encountered
+func (processor *Processor) Write(writer io.Writer) error {
+	for i, step := range processor.steps {
+		err := step.Write(writer)
+		if err != nil {
+			return fmt.Errorf("protox.Processor(writing).step(%d): %w", i, err)
+		}
+	}
+
+	return nil
+}
+
+// Continues to write even if an error is encountered, returns all errors combined
+func (processor *Processor) ForceWrite(writer io.Writer) error {
+	errs := []error{}
+
+	for i, step := range processor.steps {
+		err := step.Write(writer)
+		if err != nil {
+			errs = append(errs, fmt.Errorf("protox.Processor(writing).step(%d): %w", i, err))
+		}
+	}
+
+	return errors.Join(errs...)
 }
